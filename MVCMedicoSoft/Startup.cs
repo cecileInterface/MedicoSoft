10f337ab6c2ab5ec4231a7ae0de07cd1a2aefd7a
cecileInterface <c.bonleu@interface3.be>
2014-10-09 13:53:53 +0200

    Création projet

---
+++ MVCMedicoSoft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMedicoSoft.Startup))]
+namespace MVCMedicoSoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
